2518172c139521248004a9038c538af0e21143b2
Jennifer Spicer <xJenniferSpicer@gmail.com>
2024-02-04 16:28:59 -0600

    Changed MyBot to jmspicer

--- Jenny's Assisted Chess Bot/src/com/stephengware/java/games/chess/bot/jmspicer.java
+++ Jenny's Assisted Chess Bot/src/com/stephengware/java/games/chess/bot/jmspicer.java
@@ -12,7 +12,7 @@ import com.stephengware.java.games.chess.state.State;
  * 
  * @author Stephen G. Ware
  */
-public class MyBot extends Bot {
+public class jmspicer extends Bot {
 
 	/** A random number generator */
 	private final Random random;
@@ -21,21 +21,29 @@ public class MyBot extends Bot {
 	 * Constructs a new chess bot named "jmspicer" and whose random  number
 	 * generator (see {@link java.util.Random}) begins with a seed of 0.
 	 */
-	public MyBot() {
+	public jmspicer() {
 		super("jmspicer");
 		this.random = new Random(0);
 	}
 
 	@Override
 	protected State chooseMove(State root) {
+		/**
+		 * Here you are provided with a State whose root represents the current state of the board and must
+		 * return one of its child states. Each child state represents a reachable state given the
+		 * current board configuration. Thus returning one of these child states is essentially
+		 * expressing which move you want to make.
+		 */
 		// This list will hold all the children nodes of the root.
 		ArrayList<State> children = new ArrayList<>();
+		
 		// Generate all the children nodes of the root (that is, all the
 		// possible next states of the game.  Make sure that we do not exceed
-		// the number of GameTree nodes that we are allowed to generate.
+		// the number of GameTree nodes that we are allowed to generate... 500,000 for this assignment.
 		Iterator<State> iterator = root.next().iterator();
 		while(!root.searchLimitReached() && iterator.hasNext())
 			children.add(iterator.next());
+		
 		// Choose one of the children at random.
 		return children.get(random.nextInt(children.size()));
 	}
