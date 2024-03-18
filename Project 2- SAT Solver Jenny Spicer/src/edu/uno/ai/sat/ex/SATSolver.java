002005e56e9d2eab4febaa77e8ac22a9f6e18af6
Jennifer Spicer <xJenniferSpicer@gmail.com>
2024-03-18 12:15:31 -0500

    Added WalkSAT pseudocode

--- Project 2- SAT Solver Jenny Spicer/src/edu/uno/ai/sat/ex/SATSolver.java
+++ Project 2- SAT Solver Jenny Spicer/src/edu/uno/ai/sat/ex/SATSolver.java
@@ -1,34 +1,41 @@
 package edu.uno.ai.sat.ex;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import edu.uno.ai.sat.Assignment;
+import edu.uno.ai.sat.Clause;
+import edu.uno.ai.sat.Literal;
 import edu.uno.ai.sat.Solver;
 import edu.uno.ai.sat.Value;
 import edu.uno.ai.sat.Variable;
 
 /**
- * 
- * @author Your Name
+ * <i>Project 2: SAT Solver</i>
+ * @author Jenny Spicer
  */
 public class SATSolver extends Solver {
 
 	private final Random random = new Random(0);
 	
 	/**
-	 * Constructs a new random SAT solver. You should change the string below
-	 * from "random" to your ID. You should also change the name of this class.
-	 * In Eclipse, you can do that easily by right-clicking on this file
-	 * (RandomAgent.java) in the Package Explorer and choosing Refactor > Rename.
+	 * Constructs a new random SAT solver with the name being my UNO ID name, jmspicer.
 	 */
 	public SATSolver() {
 		super("jmspicer");
 	}
 
+	/**
+	 * //TODO: add description
+	 * 
+	 * @param assignment the assignment being worked on
+	 * @return a boolean, representing if it is solved
+	 */
 	@Override
 	public boolean solve(Assignment assignment) {
-		// If the problem has no variables, it is trivially true or false.
+		// If the problem has no variables, it is assumed to have the values true or false.
+		// edge case where there are 0 variables:
 		if(assignment.problem.variables.size() == 0)
 			return assignment.getValue() == Value.TRUE;
 		else {
@@ -38,10 +45,11 @@ public class SATSolver extends Solver {
 				Variable variable = chooseVariable(assignment);
 				// Choose 'true' or 'false' at random.
 				Value value;
-				if(random.nextBoolean())
+				if(random.nextBoolean())//randomly is true or false
 					value = Value.TRUE;
-				else
+				else {
 					value = Value.FALSE;
+				}
 				// Assign the chosen value to the chosen variable.
 				assignment.setValue(variable, value);
 			}
@@ -50,6 +58,23 @@ public class SATSolver extends Solver {
 			return true;
 		}
 	}
+	
+	/*
+	 * WALKSAT pseudocode (checks satisfiability by randomly flipping the values of variables)
+	 * 
+	 * function WALKSAT(clauses, p, max_flips) returns a satisfying model or failure
+	 * inputs: clauses, a set of clauses in propositional logic
+	 * 			p, the probability of choosing to so a "random walk" move, typically around 0.5
+	 * 			max_flips, number of flips allowed before giving up
+	 * 
+	 * model <- a random assignment of true/false to the symbols in clauses
+	 * for i = 1 to max_flips, do
+	 * 		if model satisfies clauses then return model
+	 * 		clause <- a randomly selected clause from clauses that is false in model
+	 * 		with probability p, flip the value in model of a randomly selected symbol from clause
+	 * 		else flip whichever symbol in clause maximizes the number of satisfied clauses
+	 * return failure
+	 */
 	
 	/**
 	 * Randomly choose a variable from the problem whose value will be set. If
@@ -74,4 +99,48 @@ public class SATSolver extends Solver {
 		else
 			return assignment.problem.variables.get(random.nextInt(assignment.problem.variables.size()));
 	}
+	
+	//---------------------------- HELPERS ----------------------------//
+	
+//	/**
+//	 * Finds out if any given variable is a pure symbol.
+//	 * Example: (and (or A B) (or (not A) B) [or (not A) (not B)]); // TODO: ADD explanation   .
+//	 * 
+//	 * @param literals	List of one or more Literal
+//	 * @see clauseList	clauseList helper method for getting a list of Literals from a given Clause
+//	 * @param variable	Variable to check
+//	 * @return a boolean; true if pure, otherwise false.
+//	 */
+//	public boolean pureCheck(ArrayList<Literal> literals, Variable variable) {
+//		//stubbed
+//		return true;
+//	}
+//	
+//	/**
+//	 * This is the list of each literal that shows up in a specific clause; could be used to help find unit clauses.
+//	 * Example: (or (not A) B); There are two literals: the (not A) literal, and the B literal.
+//	 * 
+//	 * @param clause	A Clause looks like this: (or (not A) B)
+//	 * @return an ArrayList containing one or more Literal.
+//	 */
+//	public ArrayList<Literal> clauseList(Clause clause){
+//		//ArrayList<Literal> list = new ArrayList<Literal>();
+//		
+//		//stubbed
+//		return new ArrayList<Literal>();
+//	}
+//	
+//	/**
+//	 * This is how you know if the literal is a “positive” literal or a “negative” literal, i.e.,
+//	 * whether the literal is negated or not. This also can be used to help find pure symbols!
+//	 * Example: (or (not A) B); The ‘A’ literal here would have a valence of ‘false’ because it is negated.
+//	 * The ‘B’ literal here would have a valence of ‘true’ because it is not negated.
+//	 * 
+//	 * @param clause	A Clause looks like this: (or (not A) B)
+//	 * @return an ArrayList containing one or more Literal.
+//	 */
+//	public boolean valence(Literal literal) {
+//		//stubbed
+//		return true;
+//	}
 }
